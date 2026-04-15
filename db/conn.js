c4e3f07990f165921673b136499fd3b06d2f8836
waqasAnjum1985 <anjum.pharmacy@gmail.com>
2026-04-15 16:25:38 +0500

    lets start

---
+++ db/conn.js
@@ -0,0 +1,26 @@
+const mongoose = require("mongoose");
+
+const db = process.env.MONGO_CONN;
+
+let isConnected;
+
+const connectDB = async () => {
+  try {
+    await mongoose.connect(db);
+    isConnected = true;
+    console.log("Mongo DB connected successfully...");
+  } catch (err) {
+    console.log("err while connecting Mongo DB", err);
+  }
+};
+const connectionDropped = (req, res, next) => {
+  if (!isConnected) {
+    connectMongo();
+  }
+  next();
+};
+
+module.exports = {
+  connectDB,
+  connectionDropped,
+};
---
+++ server.js
@@ -0,0 +1,47 @@
+//core modules
+const express = require("express");
+//imported modules
+require("dotenv").config();
+//local modules
+const app = express();
+const PORT = process.env.PORT;
+const cors = require("cors");
+const bodyParser = require("body-parser");
+const cookieParser = require("cookie-parser");
+const portfolioRouter = require("./routes/portfolioRoute.js");
+const mongoose = require("mongoose");
+
+const corsOptions = {
+  origin: "http://localhost:5173",
+  credentials: true,
+};
+app.use(cors(corsOptions));
+app.use(cookieParser());
+
+app.use(express.json());
+
+let isConnected = false;
+
+async function connectToMingoDB() {
+  try {
+    await mongoose.connect(process.env.MONGO_CONN, {
+      useNewUrlParser: true,
+      useUnifiedToppology: true,
+    });
+    isConnected = true;
+    console.log("connected to MongoDB");
+  } catch (error) {
+    console.log("error while connecting Mongose", error);
+  }
+}
+
+app.use((req, res, next) => {
+  if (!isConnected) {
+    connectToMingoDB();
+  }
+  next();
+});
+
+app.use("/api/portfolio", portfolioRouter);
+
+module.exports = app;
